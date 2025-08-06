f019792503b7fcc2cf762b005197ef53d890fa24
Akhil1015 <akhiljakkampudi15@gmail.com>
2025-08-06 12:23:12 +0000

    exxx,py updated

---
+++ exxx222.py
@@ -0,0 +1,10 @@
+git branch --set-upstream-to=origin/dev dev
+git pull
+git branch --set-upstream-to=origin/dev dev
+git pull
+git branch --set-upstream-to=origin/dev dev
+git pull
+git branch --set-upstream-to=origin/dev dev
+git pull
+git branch --set-upstream-to=origin/dev dev
+git pull
